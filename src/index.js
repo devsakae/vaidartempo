5565f522176619fd12423fcacd9414bcb1580163
TornadoZer0 <64657910+TornadoZer0@users.noreply.github.com>
2023-01-19 10:49:58 -0300

    chore: Importado globalStyles no index.js

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import GlobalStyles from '../src/style/globalStyles'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <GlobalStyles />
     <App />
   </React.StrictMode>
 );
