cacc36969d58d880c58aae299dd395c6ff1c2b57
Rodrigo Sakae <devsakae@gmail.com>
2023-02-09 17:09:52 -0300

    chore: novas ações adicionadas

--- src/conteudo/acoes.js
+++ src/conteudo/acoes.js
@@ -1,9 +1,9 @@
 //Agradecimentos especiais a: Aline Grance, Isa Costa, Gustavo Aquino, Cris (Weydson), Ernani Ávila, Washington Gama.
 
 const actions = {
-  um: ['Se esticar', 'Encher a garrafa d\'água', 'Lavar as mãos', 'Abrir a janela', 'Atender a campainha', 'Commitar as alterações', 'Responder o forms diário', 'Botar o celular pra carregar', 'Agradecer alguém'],
-  umemeio: ['Descascar uma fruta', 'Pegar um chocolatinho', 'Passar um café de máquina', 'Olhar o WhatsApp'],
-  dois: ['Trocar a foto do perfil', 'Dar uma olhada no Slack', 'Atualizar o Readme.md', 'Ler o course de novo', 'Passar 1 fase do Candy Crush', 'Fazer uma bisnaguinha com presunto e queijo'],
+  um: ['Se esticar', 'Encher a garrafa d\'água', 'Lavar as mãos', 'Abrir a janela', 'Commitar as alterações', 'Responder o forms diário', 'Botar o celular pra carregar', 'Agradecer alguém'],
+  umemeio: ['Descascar uma fruta', 'Pegar um chocolatinho', 'Passar um café de máquina', 'Olhar o WhatsApp', 'Limpar o monitor'],
+  dois: ['Dar uma olhada no Slack', 'Atualizar o Readme.md', 'Ler o course de novo', 'Passar 1 fase do Candy Crush', 'Fazer uma bisnaguinha com presunto e queijo', 'Fazer um pitch pessoal'],
   doisemeio: ['Fechar os olhos e encontrar seu centro', 'Limpar o teclado', 'Arrumar a cama', 'Ir ao banheiro (n. 1)', 'Lavar o óculos', 'Enviar um áudio para devsakae@gmail.com'],
   tres: ['Ouvir uma música', 'Fazer um miojo', 'Reiniciar o computador', 'Assistir o vídeo do Baby Shark', 'Fazer um bolo de caneca'],
   quatro: ['Esquentar uma água', 'Limpar a sujeira do cachorro', 'Estourar uma pipoca', 'Escovar os dentes', 'Esquentar um hot pocket'],
