1991c7b2c5bfad2217131515597b443aeef60978
Rodrigo Sakae <devsakae@gmail.com>
2023-03-14 13:48:33 -0300

    feat: áudio de @LucasHenriqueAbreu

--- src/conteudo/audios-falta1min.js
+++ src/conteudo/audios-falta1min.js
@@ -1,10 +1,11 @@
 // Falta 1 minuto
-import lelesakae from './audios/lelesakae.04.mp3';
-import devsakae from './audios/devsakae.mp3';
 import alinegrance from './audios/alinegrance.mp3';
-import ThiagoFdaSLopes from './audios/ThiagoFdaSLopes.01.mp3';
-import nataliakoliveira from './audios/nataliakoliveira.01.ogg';
 import apaulinhacarlos from './audios/apaulinhacarlos.03.mp4';
+import devsakae from './audios/devsakae.mp3';
+import lelesakae from './audios/lelesakae.04.mp3';
+import LucasHenriqueAbreu from './audios/LucasHenriqueAbreu.01.mp4';
+import nataliakoliveira from './audios/nataliakoliveira.01.ogg';
+import ThiagoFdaSLopes from './audios/ThiagoFdaSLopes.01.mp3';
 
 const falta1min = [
   lelesakae,
@@ -12,7 +13,8 @@ const falta1min = [
   alinegrance,
   ThiagoFdaSLopes,
   nataliakoliveira,
-  apaulinhacarlos
+  apaulinhacarlos,
+  LucasHenriqueAbreu,
 ];
 
 export default falta1min;
--- src/conteudo/audios-fimdotempo.js
+++ src/conteudo/audios-fimdotempo.js
@@ -1,17 +1,18 @@
 // Acabou o tempo
+import apaulinhacarlos01 from './audios/apaulinhacarlos.01.mp4';
+import apaulinhacarlos02 from './audios/apaulinhacarlos.02.mp4';
+import cybersekh from './audios/cybersekh.mp3';
+import ernanicavila from './audios/ernanicavila.ogg';
+import isaacost from './audios/isaacost.ogg';
 import lelesakae01 from './audios/lelesakae.01.mp3';
 import lelesakae02 from './audios/lelesakae.02.mp3';
 import lelesakae03 from './audios/lelesakae.03.mp3';
-import ernanicavila from './audios/ernanicavila.ogg';
-import isaacost from './audios/isaacost.ogg';
-import wgamaneto from './audios/wgamaneto.ogg';
-import WeydsonCristiano from './audios/WeydsonCristiano.ogg';
+import LucasHenriqueAbreu from './audios/LucasHenriqueAbreu.02.mp4';
 import maitemarques from './audios/maite-marques.mp3';
-import ThiagoFdaSLopes from './audios/ThiagoFdaSLopes.02.mp3';
-import cybersekh from './audios/cybersekh.mp3';
 import nataliakoliveira from './audios/nataliakoliveira.ogg';
-import apaulinhacarlos01 from './audios/apaulinhacarlos.01.mp4';
-import apaulinhacarlos02 from './audios/apaulinhacarlos.02.mp4'
+import ThiagoFdaSLopes from './audios/ThiagoFdaSLopes.02.mp3';
+import WeydsonCristiano from './audios/WeydsonCristiano.ogg';
+import wgamaneto from './audios/wgamaneto.ogg';
 
 const fimdotempo = [
   lelesakae01,
@@ -26,7 +27,8 @@ const fimdotempo = [
   cybersekh, 
   nataliakoliveira, 
   apaulinhacarlos01, 
-  apaulinhacarlos02
+  apaulinhacarlos02,
+  LucasHenriqueAbreu
 ];
 
 export default fimdotempo;
