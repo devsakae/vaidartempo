7fc581b1a75503195e339c8e6558c2ec73a08df4
Rodrigo Sakae <devsakae@gmail.com>
2023-08-02 09:10:51 -0300

    feat: google analytics 4

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ReactGA from 'react-ga4';
+
+ReactGA.initialize("G-D6ZDNF7BKZ")
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
